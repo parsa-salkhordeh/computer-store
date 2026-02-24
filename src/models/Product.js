e8e034fb831a5dba467f0270f06a5d6a7fa1fce4
YourUsername <parsasalkhordd@gmail.com>
2026-02-24 21:30:04 +0330

    feat: add Product model schema

---
+++ src/models/Product.js
@@ -0,0 +1,25 @@
+const { default: mongoose } = require("mongoose");
+
+const ProductSchema=new mongoose.Schema({
+    id: Number,
+    name: String,
+    price: Number,
+    category: String,
+    cpu: String,
+    ram: String,
+    image: String,
+    description: String
+})
+
+const Product= mongoose.models.Product || mongoose.model("Prodduct" , ProductSchema)
+export default Product;
+
+
+//     id: 8,
+//     name: "دل Inspiron 15",
+//     price: 23900000,
+//     category: "laptop",
+//     cpu: "Intel Core i5",
+//     ram: "8GB",
+//     image: "https://example.com/images/dell_inspiron15.jpg",
+//     description: "لپتاپ میان‌رده با صفحه نمایش بزرگ برای کارهای عمومی."
