58af3d69631269b89700df14971a92c204fc00e4
YourUsername <parsasalkhordd@gmail.com>
2026-02-25 14:14:24 +0330

    feat: fetch products data in Home page and import ProductsList component

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,22 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import HeroTopBrand from "@/components/HeroTopBrand";
+import ProductsList from "@/components/ProductsList";
 import TopBrand from "@/components/TopBrand";
 
-export default function Home() {
+
+
+export default async function Home() {
+  const res= await fetch("http://localhost:3000/api/products")
+  const data=await res.json()
+  console.log(data)
   return (
     <div>
       <Header/>
       <HeroSection/>
       <HeroTopBrand/>
       <TopBrand/>
+      <ProductsList products={data}/>
     </div>
   );
 }
