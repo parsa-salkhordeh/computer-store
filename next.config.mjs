3ace706b2d014d662dc3c09f911cbb0acb0e9fd6
YourUsername <parsasalkhordd@gmail.com>
2026-02-25 16:15:53 +0330

    chore:fixed next.config for Image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    domains: ["images.unsplash.com"],
+  },
 };
 
 export default nextConfig;
+
